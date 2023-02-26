3be8cac08f85f32f8d8014e5bde3ccdd50472b21
DESKTOP-N5R9FT2\Fritz <fritzjohn_solibio@yahoo.ca>
2023-02-26 12:55:15 -0800

    added button functionality to determine correct answer

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,13 +1,73 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defautlAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
 
     void Start()
     {
+        DisplayQuestion();
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        correctAnswerIndex = question.GetCorrectAnswerIndex();
+        Image buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+
+        if (index.Equals(correctAnswerIndex))
+        {
+            questionText.text = "Correct!";
+        } 
+        else
+        {
+            var correctAnswerText = answerButtons[correctAnswerIndex].GetComponentInChildren<TextMeshProUGUI>();
+            questionText.text = $"The correct answer was: {correctAnswerText.text}";
+        }
+
+        buttonImage.sprite = correctAnswerSprite;
+        UpdateButtonState(false);
+    }
+
+    private void GetNextQuestion()
+    {
+        UpdateButtonState(true);
+        SetDefaultButtonSprites();
+        DisplayQuestion();
+    }
+
+    private void DisplayQuestion()
+    {
         questionText.text = question.GetQuestion();
+
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    private void UpdateButtonState(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
+
+    private void SetDefaultButtonSprites()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Image buttonImage = answerButtons[i].GetComponent<Image>();
+            buttonImage.sprite = defautlAnswerSprite;
+        }
     }
 }
